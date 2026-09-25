9042f25b30c16e20b0e6a691fc6df770c5c399f5
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-25 16:21:35 +0700

    V6.4: rotate application shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pricereport-shell-v63-unified-content-workspaces';
+const CACHE = 'pricereport-shell-v64-guided-content-workspaces';
 const CORE = [
   './index.html',
   './manifest.webmanifest',
